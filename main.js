629f32d21e7562622b8a5cb27230196c5f96fa68
Noman Qureshi <164647437+noman-900@users.noreply.github.com>
2024-07-30 18:28:06 +0500

    Add files via upload
    Words Counter

---
+++ main.js
@@ -0,0 +1,21 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+console.log(chalk.bold.bgCyan("\n \t\t Words-Counter-By-Noman"));
+console.log("=".repeat(60));
+// prompt the user to enter a sentence
+let answers = await inquirer.prompt([
+    {
+        name: "sentence",
+        type: "input",
+        message: "Enter a Sentence",
+    }
+]);
+// tirmming the sentence and splitting it into words based on "spaces"
+let words = answers.sentence.trim().split(" ");
+// Analysis of user input sentence
+console.log("=".repeat(60));
+console.log(chalk.bold("- Sentence Words:"));
+console.log(words);
+console.log(chalk.bold(`\n - Word Count: ${chalk.bold.green(words.length)}`));
+console.log("=".repeat(60));
